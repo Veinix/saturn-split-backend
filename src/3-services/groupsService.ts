4f77751c11d5c1834b7bbefc41ad71e27329d7a9
David Aviles <davidaviles_dev@outlook.com>
2025-06-08 12:52:20 +0300

    test

--- src/3-services/groupsService.ts
+++ src/3-services/groupsService.ts
@@ -1,38 +1,38 @@
-import supabase from "../plugins/db";
+// import supabase from "../plugins/db";
 
-export async function getAllGroups() {
-    const { data, error } = await supabase
-        .from("groups")
-        .select("*");
-    if (error) throw error;
-    return data;
-}
+// export async function getAllGroups() {
+//     const { data, error } = await supabase
+//         .from("groups")
+//         .select("*");
+//     if (error) throw error;
+//     return data;
+// }
 
-export async function getUserGroups(
-    userId: string
-) {
-    const { data, error } = await supabase
-        .from('groups')
-        .select(`
-        id,
-        creator_id,
-        name,
-        description,
-        group_icon,
-        members:group_members(user_id),
-        transactions:transactions(
-          id,
-          lender,
-          borrower,
-          amount,
-          transaction_type,
-          transaction_date
-        )
-      `)
-        .eq('members.user_id', userId);
+// export async function getUserGroups(
+//     userId: string
+// ) {
+//     const { data, error } = await supabase
+//         .from('groups')
+//         .select(`
+//         id,
+//         creator_id,
+//         name,
+//         description,
+//         group_icon,
+//         members:group_members(user_id),
+//         transactions:transactions(
+//           id,
+//           lender,
+//           borrower,
+//           amount,
+//           transaction_type,
+//           transaction_date
+//         )
+//       `)
+//         .eq('members.user_id', userId);
 
-    if (error) {
-        throw new Error(`Could not fetch groups: ${error.message}`);
-    }
-    return data;
-}
+//     if (error) {
+//         throw new Error(`Could not fetch groups: ${error.message}`);
+//     }
+//     return data;
+// }
